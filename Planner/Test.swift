6e52bec9f68cf9359b29727a494af7812701b508
Usercherrykim <9812kmc@naver.com>
2023-03-16 17:27:02 +0900

    test

---
+++ Planner/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Planner
+//
+//  Created by binee on 2023/03/16.
+//
+
+import Foundation
+import SwiftUI
+
+struct Test: View{
+    var body: some View{
+        Text("hi")
+    }
+}
